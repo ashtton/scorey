0e4d9217ae3bcdadfb6e8eab194cb24d13297e0f
BuildTools <7jashton@gmail.com>
2021-12-03 15:59:57 -0500

    Fixed titles updating

--- src/main/java/me/gleeming/scorey/Scorey.java
+++ src/main/java/me/gleeming/scorey/Scorey.java
@@ -12,7 +12,6 @@ import org.bukkit.scoreboard.Team;
 
 import java.util.List;
 import java.util.Random;
-import java.util.UUID;
 
 /**
  * Main class
@@ -37,39 +36,41 @@ public class Scorey {
             public void run() {
                 Bukkit.getOnlinePlayers().forEach(player -> {
                     ScoreyPlayer scoreyPlayer = ScoreyPlayer.getPlayers().get(player.getUniqueId());
-                    if(scoreyPlayer == null) scoreyPlayer = new ScoreyPlayer(player.getUniqueId());
+                    if(scoreyPlayer != null) {
+                        List<String> scoreboard = ScoreyPlayer.getScoreboard(player);
+                        scoreyPlayer.getTitleObjective().setDisplayName(ChatColor.translateAlternateColorCodes('&', helper.getTitle()));
 
-                    List<String> scoreboard = ScoreyPlayer.getScoreboard(player);
-                    int i = 1;
-                    for(String line : scoreboard) {
-                        Team team = scoreyPlayer.getTeams().get(i);
+                        int i = 1;
+                        for(String line : scoreboard) {
+                            Team team = scoreyPlayer.getTeams().get(i);
 
-                        if(team == null) team = scoreyPlayer.getBukkitScoreboard().registerNewTeam(scoreyPlayer.getRandomTeamName());
-                        if(team.getEntries().size() == 0) team.addEntry(ChatColor.values()[new Random().nextInt(ChatColor.values().length)].toString() + ChatColor.values()[new Random().nextInt(ChatColor.values().length)].toString());
+                            if(team == null) team = scoreyPlayer.getBukkitScoreboard().registerNewTeam(scoreyPlayer.getRandomTeamName());
+                            if(team.getEntries().size() == 0) team.addEntry(ChatColor.values()[new Random().nextInt(ChatColor.values().length)].toString() + ChatColor.values()[new Random().nextInt(ChatColor.values().length)].toString());
 
-                        if(line.length() > 32) line = line.substring(0, 32);
+                            if(line.length() > 32) line = line.substring(0, 32);
 
-                        if(line.length() > 16) {
-                            team.setPrefix(ChatColor.translateAlternateColorCodes('&', line.substring(0, 16).endsWith("&") ? line.substring(0, 15) : line.substring(0, 16)));
-                            team.setSuffix(ChatColor.translateAlternateColorCodes('&', ChatColor.getLastColors(team.getPrefix()) + (line.substring(0, 16).endsWith("&") ? line.substring(15) : line.substring(16))));
-                        } else {
-                            team.setPrefix(ChatColor.translateAlternateColorCodes('&', line));
-                            team.setSuffix("");
+                            if(line.length() > 16) {
+                                team.setPrefix(ChatColor.translateAlternateColorCodes('&', line.substring(0, 16).endsWith("&") ? line.substring(0, 15) : line.substring(0, 16)));
+                                team.setSuffix(ChatColor.translateAlternateColorCodes('&', ChatColor.getLastColors(team.getPrefix()) + (line.substring(0, 16).endsWith("&") ? line.substring(15) : line.substring(16))));
+                            } else {
+                                team.setPrefix(ChatColor.translateAlternateColorCodes('&', line));
+                                team.setSuffix("");
+                            }
+
+                            scoreyPlayer.getTitleObjective().getScore(team.getEntries().stream().findFirst().get()).setScore(i);
+                            scoreyPlayer.getTeams().put(i, team);
+
+                            i++;
                         }
 
-                        scoreyPlayer.getTitleObjective().getScore(team.getEntries().stream().findFirst().get()).setScore(i);
-                        scoreyPlayer.getTeams().put(i, team);
-
-                        i++;
-                    }
-
-                    for(int x = i; x < 20; x++) {
-                        Team team = scoreyPlayer.getTeams().get(x);
-                        if(team != null) {
-                            scoreyPlayer.getTeams().remove(x);
-                            if(team.getEntries().size() > 0) {
-                                scoreyPlayer.getBukkitScoreboard().resetScores(team.getEntries().stream().findFirst().get());
-                                team.unregister();
+                        for(int x = i; x < 20; x++) {
+                            Team team = scoreyPlayer.getTeams().get(x);
+                            if(team != null) {
+                                scoreyPlayer.getTeams().remove(x);
+                                if(team.getEntries().size() > 0) {
+                                    scoreyPlayer.getBukkitScoreboard().resetScores(team.getEntries().stream().findFirst().get());
+                                    team.unregister();
+                                }
                             }
                         }
                     }
--- src/main/java/me/gleeming/scorey/listener/ScoreboardListener.java
+++ src/main/java/me/gleeming/scorey/listener/ScoreboardListener.java
@@ -1,11 +1,17 @@
 package me.gleeming.scorey.listener;
 
+import me.gleeming.scorey.Scorey;
 import me.gleeming.scorey.player.ScoreyPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class ScoreboardListener implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent e) { ScoreyPlayer.getPlayers().remove(e.getPlayer().getUniqueId()); }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) { Bukkit.getScheduler().runTaskLater(Scorey.getInstance().getPlugin(), () -> new ScoreyPlayer(e.getPlayer().getUniqueId()), 2); }
 }
